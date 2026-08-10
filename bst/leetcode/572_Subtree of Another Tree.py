e06b8584fc277debe927f323252c3b6e2a23c812
sdan-07 <soumyadipdan7@gmail.com>
2026-08-10 20:56:22 +0530

    done

---
+++ bst/leetcode/572_Subtree of Another Tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        return self.isSubRoot(root, subRoot)
+
+    def isSubRoot(self, p, q):
+        if not p: return False
+        if self.isSameTree(p, q):
+            return True
+
+        return self.isSubRoot(p.left, q) or self.isSubRoot(p.right, q)
+
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
