2caa8f9d17897e51466e8eb2207ef0e5a4688636
sdan-07 <soumyadipdan7@gmail.com>
2026-09-05 13:39:51 +0530

    done

---
+++ blind 75/group anagrams_apr2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = {}
+        for s in strs:
+            freq = self.frequency(s)
+            anag = []
+            if freq not in d:
+                anag.append(s)
+                d[freq] = anag
+            else:
+                d.get(freq).append(s)
+
+        res = []
+        for k, v in d.items():
+            res.append(v)
+
+        return res
+
+    def frequency(self, s):
+        lst = [0] * 26
+
+        for ch in s:
+            lst[ord(ch) - ord("a")] += 1
+
+        return "#".join(map(str, lst))
+
